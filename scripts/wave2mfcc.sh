e99bf6350b4cf297d5ea0922cbf857fe75b839c2
ksingla025 <ksingla025@gmail.com>
2015-06-18 01:39:27 +0530

    Update wave2mfcc.sh

--- scripts/wave2mfcc.sh
+++ scripts/wave2mfcc.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                   
 
-wave=$1
-dir=`dirname $wave`
-base=`basename $wave .wav`
-
-java -Xmx2048m -classpath ./LIUM/lium.jar fr.lium.spkDiarization.tools.Wave2FeatureSet --help --fInputMask=$wave --sInputMask="" --fInputDesc="audio16kHz2sphinx,1:1:0:0:0:0,13,0:0:0" --fOutputMask=$dir/$base.mfcc --fOutputDesc="audio16kHz2sphinx,1:1:0:0:0:0,13,0:0:0" $base
-
+sph=$1                                                                                                                                        
+mfcc=$2                                                                                                                                       
+tools/spro-4.0/sfbcep -v -F PCM16 -p 12 -m -e -f 16000 $sph $mfcc.tmp
+tools/spro-4.0/binary/bin/scopy -B $mfcc.tmp $mfcc
+rm $mfcc.tmp
